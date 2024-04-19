65b7725f62fe5250c9ff765c96c413a6464ea9af
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-04-19 19:49:40 -0300

    feat: criada nova controller para criar todo

---
+++ src/controllers/Todo/create-todo.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import prismaClient from "../../services/prisma";
+
+export class CreateTodoController {
+  async handle(req: Request, res: Response) {
+    try {
+      const userId = req.userId;
+      const { title, description } = req.body;
+
+      const user = await prismaClient.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+
+      const Todo = await prismaClient.todo.create({
+        data: {
+          title,
+          description,
+          userId: userId,
+        },
+      });
+
+      return res.status(201).json({ message: "Todo criado com sucesso", Todo });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro ao criar todo" });
+    }
+  }
+}
