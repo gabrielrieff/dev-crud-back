306d05205f9abd8ffc25dc78e35bf984f754562f
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-05-11 21:13:14 -0300

    feat: Adicionado toda parte de detalhes de usuario

---
+++ src/use-case/user/detail-user/detail-user.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { InMemoryRepository } from "../../../repositories/user/implementations/in-memory-repository";
+import { CreateUserUseCase } from "../create-user/create-user-use-case";
+import { DetailUserUseCase } from "./detail-user-use-case";
+import { User } from "../../../entities/user/user";
+
+describe("Detail user", () => {
+  const inMemoryRepository = new InMemoryRepository();
+  const createUserUseCase = new CreateUserUseCase(inMemoryRepository);
+  const detailUser = new DetailUserUseCase(inMemoryRepository);
+
+  it("should be able to detail is user", async () => {
+    const user = await createUserUseCase.execute({
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@gmail.com",
+      password: "123123",
+    });
+
+    expect(detailUser.execute({ id: user.id })).resolves.toBeInstanceOf(User);
+  });
+});
