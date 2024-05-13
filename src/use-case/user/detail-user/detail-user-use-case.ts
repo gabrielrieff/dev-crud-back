a9153ec137b2a2e671b945500cd7fcb748b261ee
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-05-13 13:10:45 -0300

    refactor

--- src/use-case/user/detail-user/detail-user-use-case.ts
+++ src/use-case/user/detail-user/detail-user-use-case.ts
@@ -6,7 +6,7 @@ export class DetailUserUseCase {
   constructor(private userRepository: IUserRepository) {}
 
   async execute({ id }: IDetailUserDTO): Promise<Omit<User, "password">> {
-    const user = await this.userRepository.findOverlappingUserById(id);
+    const user = await this.userRepository.detail(id);
     if (!user) {
       throw new Error("User was not found");
     }
